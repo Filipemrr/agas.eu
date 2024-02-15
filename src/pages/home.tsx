f6d40d35c5894a542e8627a3b0fcd8ace9a27714
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-02-15 16:28:35 -0300

    first commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Minha Pagina</h1>
+            <p> Esta e uma pagina basica criada com TypeScript! </p>
+        </div>
+    );
+}
+
+export default Home;
